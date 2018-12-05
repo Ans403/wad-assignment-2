f395ae08dca4d7698095dae731a5e0f828c15b2a
Ans403 <40587715+Ans403@users.noreply.github.com>
2018-12-05 19:50:32 +0500

    my assignment 2

---
+++ index.php
@@ -0,0 +1,214 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>A2</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="UCP Section H Assignment 2">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Ans">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/png" href="images/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap4.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="js/main.js"></script> 
+</head>
+<body>
+
+<div class="top">
+<div class="col-4" style="float:left;">
+
+    <a>UCP,Lahore</a>
+     <a>Contact us:+(92) 90078601</a>
+</div>
+<div class="col-8" style="text-align: right;">
+    <a>OUT TEAM| FAQ</a>
+    <a href="#"><i class="fab fa-facebook-f"></i> </a>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+
+</div>
+
+</div>
+<header class="jumbotron text-center">
+	<div class="container">
+		<div class="row">
+            <div class="logo col-4">
+                <img src="images/UcpBlackLogo.png" width="350px">
+            </div>
+
+            <div class="menuclr col-8">
+
+                <ul>
+                    <li><div class="dropdown">
+                            <button class="dropbtn">Elements</button>
+                            <div class="dropdown-content">
+                                <a href="#">L-1</a>
+                                <a href="#">L-2</a>
+                                <a href="#">L-3</a>
+                            </div>
+                        </div></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Pages</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="index.php">Home</a></li>
+                </ul>
+            </div>
+        </div>
+			<h1>Hi UCP</h1><br>
+            <h3>Assignment 2</h3>
+		</div>
+	</div>
+</header>
+
+<br>
+<br>
+
+<div class="container main">
+    <h2 style="text-align: center;">What can I do?</h2><br><br>
+  <div class="row">
+
+
+
+        <div class="col-md-4">
+            <h3>Fully Responsive</h3>
+            <p>
+                Be comfortable; be you. What's most important is
+                to understand what suits your body type and personality.
+                Stick to that, and you can't go wrong!
+
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h3>Powerful Portfolios </h3>
+            <p>Two things define your personality,
+                the way you manage things when you have nothing,
+                and the way you behave when you have everything.</p>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Amazing Shortcodes</h3>
+            <p>You've gotta dance like there's nobody watching,
+                Love like you'll never be hurt,
+                Sing like there's nobody listening,
+                And live like it's heaven on earth.</p>
+        </div>
+
+  </div>
+    <br>
+    <br>
+    <h2 style="text-align: center;">A Little Bit About me</h2><br>
+    <br>
+    <div class="row">
+
+           <div class="col-4" style="float:left;">
+               <img src="images/i2.jpg">
+           </div>
+
+           <div class="col-8">
+
+           <div class="p">
+
+           <h4>I am creative eget at lorem ispum dolar sit lorem. Lorem ipsum dolor sit amet,
+           consectuer ipsum adipising slit.Aenean commodo ligula eget aredolar enean</h4><br>
+           <p>
+               To discover content on the web, search engines use web crawlers that follow hyperlinks
+               through known protocol virtual port numbers. This technique is ideal for discovering
+               content on the surface web but is often ineffective at finding deep web content.
+               For example, these crawlers do not attempt to find dynamic pages that are the result of
+               database queries due to the indeterminate number of queries that are possible.<br>
+
+           </p>
+       </div>
+       </div>
+       </div>
+    <br>
+    <br>
+    <h2 style="text-align: center";>Say Hello</h2><br>
+    <br>
+
+    <div class="row">
+
+    <div class="col-6">
+        <h3>Add a comment</h3><br>
+		<form name="RegForm" action="" onsubmit="return formvalidatefunc()" method="post">  
+			<div class="form-group row"> 
+				<label class="col-sm-2 col-form-label">Name</label>
+				<div class="col-sm-10">
+					<input type="text" size=65 name="Name" class="form-control">
+				</div>
+			</div>					
+			<div class="form-group row"> 
+				<label class="col-sm-2 col-form-label">E-mail</label>
+				<div class="col-sm-10">
+					<input type="text" size=65 name="EMail" class="form-control">
+				</div>
+			</div>
+
+			<div class="form-group row"> 
+				<label class="col-sm-2 col-form-label">Message</label>
+				<div class="col-sm-10">
+					<textarea cols="55" name="Comment" class="form-control"></textarea>
+				</div>
+			</div>
+			<div class="form-group row">				
+				<div class="col-sm-2">
+					<button type="submit" name="Submit" class="btn btn-primary">Sumbit</button>
+				</div>
+
+			</div>	
+			
+			</p>          
+		</form>
+    </div>
+    <div class="col-6">
+
+        <h3>Contact Info </h3><br>
+
+
+        <p>
+            <b>Address:</b> UCP Johar Town, Lahore
+            <hr>
+            <b>Mobile:</b> 03228022175 <br>
+           <hr>
+            <b>Mobile:</b> +92 228022175<br>
+           <hr>
+            <b>Email:</b> ansbinzahid@gmail.com
+        </p>
+
+    </div>
+</div>
+
+    </div>
+  </div>
+</div>
+
+<br>
+<br>
+
+<footer class="jumbotron text-center">
+
+    <div class="row">
+        <div class="col-3"> <a href="#"><b>About me</b></a><br>
+            <a href="#">5th semester BSCS Student</a></div><br>
+
+        <div class="col-3"> <a href="#"><b>Connect with me</b></a><br>
+            <a href="#">Instagram</a><br>
+            <a href="#">Facebook</a><br>
+            <a href="#">Twiter</a><br></div>
+        <div class="col-3"> <a href="#"><b>Additional Links</b></a><br>
+            <a href="#">Project</a><br>
+            <a href="#">Pages</a><br>
+            <a href="#">Shop</a><br></div>
+        <div class="col-3"> <a href="#"><b>Contact Me</b></a><br>
+            <p>Ucp johar town, Lahore<br> 03228022175<br>ansbinzahid4@gmail.com</p></div><br>
+
+
+    </div>
+    <br>
+
+	<p>Copyright 2018 Ans</p>
+</footer>
+</body>
+</html>
